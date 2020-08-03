0b04bfd69f0d93eea45a0d979fc51f3b3a0572a8
ningbo <767968585@qq.com>
2020-08-03 09:59:14 +0800

    status
    加入状态码

--- MES-FQC/mes_V2.0.py
+++ MES-FQC/mes_V2.0.py
@@ -3,6 +3,7 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.keys import Keys
 import time
 import re
+import math
 
 def sn_read(file_str):  # read sn from file(sn.txt),return sn list
     sn_list=[]
@@ -35,15 +36,35 @@ def load(sn):
     browser.find_element_by_css_selector('button[onclick="doSave()"]').click()
     time.sleep(3)
     browser.switch_to.default_content()
-    finish=browser.find_element_by_css_selector("#log>p:nth-last-child(1)").text
-    if "完工" in finish:
-        f=True
+    status=0
+    n=browser.find_elements_by_css_selector('div>p:nth-last-child(3)>font')
+    if n==10:
+        status=1000
+        x=browser.find_elements_by_css_selector('div>p:nth-last-child(3)>font[color="#00AA00"]')
+        y=browser.find_elements_by_css_selector('div>p:nth-last-child(3)>font[color="#0000FF"]')
+        if x>=3 and x<=7:
+            status=status+x*100+y*10
+        else:
+            status=4000
+        finish=browser.find_element_by_css_selector("#log>p:nth-last-child(3)").text
+        if "完工" in finish:
+            status=status+1
+        elif "前工序" in finish:
+            status=status+2
+        else:
+            status=4000
     else:
-        f=False
-    x=browser.find_elements_by_css_selector('div>p:nth-child(2)>font[color="#00AA00"]')
-    y=browser.find_elements_by_css_selector('div>p:nth-child(2)>font[color="#0000FF"]')
-    z=browser.find_elements_by_css_selector('div>p:nth-child(2)>font[color="#000000"]')
-    return (len(x),len(y),len(z),f)
+        status=4000
+
+    # finish=browser.find_element_by_css_selector("#log>p:nth-last-child(1)").text
+    # if "完工" in finish:
+    #     f=True
+    # else:
+    #     f=False
+    # x=browser.find_elements_by_css_selector('div>p:nth-child(2)>font[color="#00AA00"]')
+    # y=browser.find_elements_by_css_selector('div>p:nth-child(2)>font[color="#0000FF"]')
+    # z=browser.find_elements_by_css_selector('div>p:nth-child(2)>font[color="#000000"]')
+    # return (len(x),len(y),len(z),f)
 
 def finish(sn):
     browser.switch_to.default_content()
@@ -69,117 +90,6 @@ if __name__ == "__main__":
     process=["1272015079198396416","1272015655940362240","1272015794260119552",\
     "1272015911050514432","1272849679634128896"]
     log(username,password)
-    for sn in sn_list:
-        t=load(sn)
-        m=t[0]
-        n=t[1]
-        f=t[3]
-        while True:
-            if m==3 and n==0:
-                log(username,password,process[0])
-                if load(sn):
-                    m=3
-                    n=1
-                if finish(sn):
-                    m=4
-                    n=0
-            elif m==3 and n==1:
-                log(username,password,process[0])
-                if finish(sn):
-                    m=4
-                n=0
 
-            elif m==4 and n==0:
-                log(username,password,process[1])
-                if load(sn):
-                    m=4
-                    n=1
-                if finish(sn):
-                    m=5
-                    n=0
-            elif m==4 and n==1:
-                log(username,password,process[1])
-                if finish(sn):
-                    m=5
-                n=0
-
-            elif m==5 and n==0:
-                log(username,password,process[2])
-                if load(sn):
-                    m=5
-                    n=1
-                if finish(sn):
-                    m=6
-                    n=0
-            elif m==5 and n==1:
-                log(username,password,process[2])
-                if finish(sn):
-                    m=6
-                n=0
-
-            elif m==6 and n==0:
-                log(username,password,process[3])
-                if load(sn):
-                    m=6
-                    n=1
-                if finish(sn):
-                    m=7
-                    n=0
-            elif m==6 and n==1:
-                log(username,password,process[3])
-                if finish(sn):
-                    m=7
-                n=0
-            
-            elif m==7 and n==0:
-                log(username,password,process[4])
-                if load(sn):
-                    m=7
-                    n=1
-                if finish(sn):
-                    m=8
-                    n=0
-            elif m==7 and n==1:
-                log(username,password,process[4])
-                if finish(sn):
-                    m=8
-                n=0
-
-            elif m==8 and n==0:
-                log(username,password,process[5])
-                if load(sn):
-                    m=8
-                    n=1
-                if finish(sn):
-                    m=9
-                    n=0
-            elif m==8 and n==1:
-                log(username,password,process[5])
-                if finish(sn):
-                    m=9
-                n=0
-
-            elif m==9 and n==0:
-                log(username,password,process[6])
-                if load(sn):
-                    m=9
-                    n=1
-                if finish(sn):
-                    m=10
-                    n=0
-            elif m==9 and n==1:
-                log(username,password,process[6])
-                if finish(sn):
-                    m=10
-                n=0    
-
-            elif m==10:
-                print(sn+" is OK")
-                i+=1
-                break
-            else:
-                print(sn+" is NO_pass")
-                break
-print("Total: {} is NO_pass".format(len(sn_list)-i))
 browser.quit()
 
